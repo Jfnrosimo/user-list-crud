f07293b39cfa78aac6abe1082ab77cf0e31e28a8
jrosi <j.rosimo098@gmail.com>
2022-12-28 19:55:32 +0800

    Feat: added features for redux user slice

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+//Import redux
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    // users:,
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
